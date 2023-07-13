8afc89adb6f26729072c42252e25b64534cbe2bc
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-07-13 20:34:40 +0300

    ders10 added

---
+++ Ders10 - 13 Temmuz/kod.js
@@ -0,0 +1,13 @@
+console.log("ilk js kodu!");
+console.log("tugay tuna");
+
+var isim = "tugay";
+var yas = 18;
+
+let sacRengi = "kırmızı";
+
+isim = "tuna";
+
+console.log(isim);
+console.log(yas);
+console.log(sacRengi);
