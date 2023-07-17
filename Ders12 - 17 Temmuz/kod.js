e21f304d285c9dc676eb74e86b4b8426dfa690c9
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-07-17 20:55:27 +0300

    ders12 added

---
+++ Ders12 - 17 Temmuz/kod.js
@@ -0,0 +1,14 @@
+let isim = "tugay";
+let soyisim = "tuna";
+let yas = 50;
+let adres = "İstanbul";
+
+let ornek = true;
+
+if (yas == 18) {
+  console.log("Yaşınız 18!");
+} else {
+  console.log("Yaş 18 değil!");
+}
+
+console.log("testtest");
---
+++ Ders12 - 17 Temmuz/kod4.js
@@ -0,0 +1,12 @@
+let isim = "tugay";
+let soyisim = "tuna";
+
+let fullName = isim + " " + soyisim;
+
+isim = "ataberk";
+
+console.log(fullName);
+
+fullName = isim + " " + soyisim;
+
+console.log(fullName);
