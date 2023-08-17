dea184a6cd1bc7f5cfae77a5fe184d02775e2962
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-08-17 20:48:19 +0300

    ders37 added

--- Ders37 - 17 Agustos/api-kullanimi/src/App.js
+++ Ders37 - 17 Agustos/api-kullanimi/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  let userURL = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    veriYukle();
+  }, []);
+
+  function veriYukle() {
+    axios
+      .get(userURL)
+      .then((item) => {
+        console.log(item.data);
+        setUsers(item.data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Kullanıcılar</h1>
+      {error && <h2>Sayfa Yüklenemedi...</h2>}
+      {loading && <h2>Yükleniyor...</h2>}
+      {users.map((item) => (
+        <div>
+          <h4 key={item.id}>{item.name}</h4>
+          <h6>{item.email}</h6>
+        </div>
+      ))}
     </div>
   );
 }
