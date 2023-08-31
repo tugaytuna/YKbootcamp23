47b317f3dd346357ed3d3e29eb9975d736b6fd15
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-08-31 20:33:20 +0300

    update

--- Ders46 - 31 Agustos/todolist/src/App.js
+++ Ders46 - 31 Agustos/todolist/src/App.js
@@ -23,9 +23,13 @@ function App() {
   }, [tasks]);
 
   function saveTask() {
-    let tempTask = { id: tasks.length + 1, task: input1Value, done: false };
-    setInput1Value("");
-    setTasks([...tasks, tempTask]);
+    if (input1Value == "") {
+      alert("Please write your task!");
+    } else {
+      let tempTask = { id: tasks.length + 1, task: input1Value, done: false };
+      setInput1Value("");
+      setTasks([...tasks, tempTask]);
+    }
   }
 
   function doneTask(value, id) {
@@ -48,9 +52,9 @@ function App() {
     <div className="appMain">
       <h1>To-Do List App</h1>
 
-      {tasks.map((item, index) => (
+      {tasks.map((item) => (
         <Task
-          key={index}
+          key={item.id}
           taskname={item.task}
           done={item.done}
           id={item.id}
@@ -58,6 +62,7 @@ function App() {
         />
       ))}
       <br />
+
       <input
         onChange={(e) => setInput1Value(e.target.value)}
         value={input1Value}
@@ -65,12 +70,30 @@ function App() {
         name=""
         id=""
         placeholder="write your task..."
+        maxLength={25}
       />
+      <label
+        className={input1Value.length >= 20 ? "warning" : "limit"}
+        htmlFor=""
+      >
+        {input1Value.length}/25
+      </label>
       <button onClick={saveTask}>Add Task</button>
+
       <br />
-      <label htmlFor="">Total Tasks: {tasks.length}</label>
+
+      <label className="taskCount" htmlFor="">
+        Total Tasks: {tasks.length}
+      </label>
+      <label className="taskCount" htmlFor="">
+        {" "}
+        --{" "}
+      </label>
+      <label className="taskCount" htmlFor="">
+        Completed Tasks: {completedTasks}
+      </label>
       <br />
-      <label htmlFor="">Completed Tasks: {completedTasks}</label>
+      <progress value={completedTasks} max={tasks.length}></progress>
     </div>
   );
 }
